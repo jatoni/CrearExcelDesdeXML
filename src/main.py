155065d632a68d8676cf367347b8b6ea50c23331
jatoni <jat_antonio_torres.juan@outlook.com>
2025-08-19 10:19:05 -0600

    Se agregan nuevos cambios para otro uso

--- src/main.py
+++ src/main.py
@@ -1,24 +1,16 @@
 import pandas as pd
-import xml.etree.ElementTree as ET
 import os
-import pprint
 import xmltodict
 from openpyxl import load_workbook
-from pathlib import Path
-from openpyxl.styles import Alignment, Border, Side, Font
 from openpyxl.utils.dataframe import dataframe_to_rows
-from openpyxl.utils import get_column_letter
 import re
+import pprint
 
-responsables_por_clave = {
-    "150901": "Mtro. Luis Ernesto Gutiérrez Martínez",
-    "010162": "Mtra. Lilibeth Hernandez Alva",
-    "010157": "Mtra. Lilibeth Hernandez Alva"
-}
+registro_alumnos = {}
 
-def readXMLAndBuildData():
+def readXMLAndBuildData(archivoXML, isPaginate):
     dataBuilded = {}
-    base_path = './ArchivosXML'
+    base_path = archivoXML
     carpetas = os.listdir(base_path)
 
     max_alumnos_por_hoja = 26
@@ -26,58 +18,95 @@ def readXMLAndBuildData():
     for carpeta in carpetas:
         ruta_carpeta = os.path.join(base_path, carpeta)
         if os.path.isdir(ruta_carpeta):
-            archivos = sorted(os.listdir(ruta_carpeta))
-            hoja_num = 1  # empezamos en hoja 1
+            archivos = sorted(os.listdir(ruta_carpeta)) 
             name_file_base = os.path.basename(carpeta)
-
-            for archivo in archivos:
-                if not archivo.endswith('.xml'):
-                    continue
-
-                # comprobar antes si la hoja actual ya está llena
-                name_file = f"{name_file_base} Hoja {hoja_num}"
-                if len(dataBuilded.get(name_file, [])) >= max_alumnos_por_hoja:
-                    hoja_num += 1
-                    name_file = f"{name_file_base} Hoja {hoja_num}"
-
-                ruta_archivo = os.path.join(ruta_carpeta, archivo)
-                folio_Control = os.path.splitext(os.path.basename(archivo))[0]
-
-                with open(ruta_archivo, 'r', encoding='utf-8') as f:
-                    contenido_xml = f.read()
-
-                diccionarioData = xmltodict.parse(contenido_xml)["TituloElectronico"]
-
-                nombreAlumno = (
-                    diccionarioData["Profesionista"]["@nombre"] + " " +
-                    diccionarioData["Profesionista"]["@primerApellido"] + " " +
-                    diccionarioData["Profesionista"]["@segundoApellido"]
-                )
-                curp = diccionarioData["Profesionista"]["@curp"]
-                programa = diccionarioData["Carrera"]["@nombreCarrera"]
-                lugar_expedicion = diccionarioData["Expedicion"]["@entidadFederativa"]
-                fecha_expedicion = diccionarioData["Expedicion"]["@fechaExpedicion"]
-                rvoe = diccionarioData["Carrera"]["@numeroRvoe"]
-                clave_insitucion = diccionarioData["Institucion"]["@cveInstitucion"]
-                name_clave = diccionarioData["Carrera"]["@cveCarrera"]
-
-                registro = {
-                    "NUM_PROG": len(dataBuilded.get(name_file, [])) + 1,
-                    "ALUMNO": nombreAlumno,
-                    "CURP": curp,
-                    "PROGRAMA": programa,
-                    "CLAVE_DE_CARRERA": name_clave,
-                    "FOLIO_DE_CONTROL": folio_Control,
-                    "LUGAR_DE_EXPEDICION": lugar_expedicion,
-                    "FECHA_DE_EXPEDICION": fecha_expedicion,
-                    "RVOE": rvoe,
-                    "CLAVE_DE_INSTITUCION": clave_insitucion,
-                    "FOLIO_DIGITAL": diccionarioData["Autenticacion"]["@folioDigital"],
-                }
-
-                dataBuilded.setdefault(name_file, []).append(registro)
-
+            if isPaginate:
+                dataBuilded = buildDataPaginate(archivos, name_file_base, max_alumnos_por_hoja, dataBuilded, ruta_carpeta)
+            else:
+                dataBuilded = buildDataWithoutPaginate(archivos, name_file_base, dataBuilded, ruta_carpeta)
     return dataBuilded
+
+def buildDataWithoutPaginate(archivos, name_file_base, dataBuilded, ruta_carpeta):
+    for archivo in archivos:
+        if not archivo.endswith('.xml'):
+            continue
+        name_file = f"{name_file_base}"
+        ruta_archivo = os.path.join(ruta_carpeta, archivo)
+        folio_Control = os.path.splitext(os.path.basename(archivo))[0]
+
+        with open(ruta_archivo, 'r', encoding='utf-8') as f:
+            contenido_xml = f.read()
+
+        diccionarioData = xmltodict.parse(contenido_xml)["Dec"]
+        #pprint.pprint(diccionarioData)
+        alumno = diccionarioData["Alumno"]
+
+        partes = [
+            alumno.get("@nombre", ""),
+            alumno.get("@primerApellido", ""),
+            alumno.get("@segundoApellido", "")
+        ]   
+        nombreAlumno = " ".join(p for p in partes if p)
+        
+        curp = diccionarioData["Alumno"]["@curp"]
+        carrera = diccionarioData["Carrera"]["@nombreCarrera"]
+        name_clave = diccionarioData["Carrera"]["@claveCarrera"]
+
+        registro = {
+            "ALUMNO": nombreAlumno,
+            "CURP": curp,
+            "Carrera": carrera,
+            "CLAVE_DE_CARRERA": name_clave,
+            "FOLIO_DE_CONTROL": folio_Control
+        }
+        dataBuilded.setdefault(name_file, []).append(registro)
+    return dataBuilded
+
+def buildDataPaginate(archivos, name_file_base, max_alumnos_por_hoja, dataBuilded, ruta_carpeta):
+    hoja_num = 1
+    for archivo in archivos:
+        if not archivo.endswith('.xml'):
+            continue
+        name_file = f"{name_file_base} Hoja {hoja_num}"
+        if len(dataBuilded.get(name_file, [])) >= max_alumnos_por_hoja:
+            hoja_num += 1
+            name_file = f"{name_file_base} Hoja {hoja_num}"
+
+        ruta_archivo = os.path.join(ruta_carpeta, archivo)
+        folio_Control = os.path.splitext(os.path.basename(archivo))[0]
+
+        with open(ruta_archivo, 'r', encoding='utf-8') as f:
+            contenido_xml = f.read()
+
+        diccionarioData = xmltodict.parse(contenido_xml)["TituloElectronico"]
+
+        nombreAlumno = (
+            diccionarioData["Profesionista"]["@nombre"] + " " +
+            diccionarioData["Profesionista"]["@primerApellido"] + " " +
+            diccionarioData["Profesionista"]["@segundoApellido"]
+        )
+        curp = diccionarioData["Profesionista"]["@curp"]
+        programa = diccionarioData["Carrera"]["@nombreCarrera"]
+        lugar_expedicion = diccionarioData["Expedicion"]["@entidadFederativa"]
+        fecha_expedicion = diccionarioData["Expedicion"]["@fechaExpedicion"]
+        rvoe = diccionarioData["Carrera"]["@numeroRvoe"]
+        clave_insitucion = diccionarioData["Institucion"]["@cveInstitucion"]
+        name_clave = diccionarioData["Carrera"]["@cveCarrera"]
+
+        registro = {
+            "NUM_PROG": len(dataBuilded.get(name_file, [])) + 1,
+            "ALUMNO": nombreAlumno,
+            "CURP": curp,
+            "PROGRAMA": programa,
+            "CLAVE_DE_CARRERA": name_clave,
+            "FOLIO_DE_CONTROL": folio_Control,
+            "LUGAR_DE_EXPEDICION": lugar_expedicion,
+            "FECHA_DE_EXPEDICION": fecha_expedicion,
+            "RVOE": rvoe,
+            "CLAVE_DE_INSTITUCION": clave_insitucion,
+            "FOLIO_DIGITAL": diccionarioData["Autenticacion"]["@folioDigital"],
+        }
+    return dataBuilded.setdefault(name_file, []).append(registro)
 
 def agregar_hoja_nueva_excel(ruta_excel, dic):
     coutn = 0
@@ -125,10 +154,10 @@ def agregar_hoja_nueva_excel(ruta_excel, dic):
         
         
         nueva_hoja.merge_cells("C51:D51")
-        # Firma izquierda
+        
         nueva_hoja["C51"] = responsable
 
-        # Firma derecha
+        
         nueva_hoja["I51"] = "Mtra. Dely Karolina Urbano Sanchez"
         nueva_hoja["I52"] = "RECTOR"
 
@@ -136,9 +165,25 @@ def agregar_hoja_nueva_excel(ruta_excel, dic):
         coutn += 1
     print(f"Total de hojas agregadas: {coutn}")
     libro.save(ruta_excel)
+    
+def obtenerDatosAlumnos(data):
+    print("Obteniendo Datos de Alumnos...")
+    ruta = "./DocuemntosTitulos/alumnos.xlsx"
+    with pd.ExcelWriter(ruta, engine="openpyxl") as writer:
+        for nombre_hoja, lista_dicts in data.items():
+            df = pd.DataFrame(lista_dicts)
+            df.to_excel(writer, sheet_name=nombre_hoja, index=False)
 
     
-ruta = "/Users/juanantoniotorres/Documents/ProyectoSalvandoVidaJacqueline/DocuemntosTitulos/Libro de Control de Folios de Titulos y Grados Electrónicos 2025.xlsx"
-registro_alumnos = readXMLAndBuildData()
-dic = registro_alumnos
-agregar_hoja_nueva_excel(ruta, dic)
+
+opc = 0
+while opc != 3:
+    opc = int(input("Menú \n1.- Generar Folio de titulos\n2.-Obtener datos de alumnos y enlistarlos\n3.-Salir\nChoose one: "))
+    if opc == 1:
+        ruta = "/Users/juanantoniotorres/Documents/ProyectoSalvandoVidaJacqueline/DocuemntosTitulos/Libro de Control de Folios de Titulos y Grados Electrónicos 2025.xlsx"
+        registro_alumnos = readXMLAndBuildData("./ArchivosXML", True)
+        dic = registro_alumnos
+        agregar_hoja_nueva_excel(ruta, dic)
+    elif opc == 2:
+        registro_alumnos = readXMLAndBuildData("./ArchivosXMLObtenerDatos", False)
+        obtenerDatosAlumnos(registro_alumnos)
